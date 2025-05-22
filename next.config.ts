de4db50771954e6f3a596fdec536d90b9a9ae3ae
Alberto Araujo <albertoaraujo.dev@gmail.com>
2025-05-22 12:07:48 -0300

    fix: add basePath to show images correctly

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    basePath: '/portfolio',
+    output: 'export',
     images: {
         remotePatterns: [
             {
