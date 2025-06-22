b11a61e6d7a60e6b52ab726b9a7e67b984ae37da
Alberto Araujo <albertoaraujo.dev@gmail.com>
2025-06-22 14:04:42 -0300

    feat(contents): add experiences data file

---
+++ src/contents/experiences.ts
@@ -0,0 +1,16 @@
+import { Experience } from "@/types";
+
+export const experiences: Experience[] = [
+    {
+        id: 'freelancer',
+        company: 'Freelancer',
+    },
+    {
+        id: 'coop',
+        company: 'COOP',
+    },
+    {
+        id: 'target',
+        company: 'Target',
+    },
+  ];
